90053850725a2bf6d288c80d122ee86fa50fc0cc
Kun-Hong, Liao <liaokunhong9700125@gmail.com>
2026-01-11 22:00:28 +0800

    Solve、Invert and Determinant using Cholesky Decomposition

--- src/Coleskyfunction.cpp
+++ src/Coleskyfunction.cpp
@@ -4,7 +4,7 @@ using namespace Rcpp;
 
 // [[Rcpp::export]]
 
-List CholeskyDecompose(NumericMatrix mat){
+List CholDecompose(NumericMatrix mat){
   int n = mat.nrow(); 
   if(mat.ncol() != n) stop("Matrix must be squared. "); 
   
@@ -33,4 +33,86 @@ List CholeskyDecompose(NumericMatrix mat){
     Named("L") = L
   ); 
 }
+
+// [[Rcpp::export]]
+NumericVector CholSolve(List chol_result, NumericVector b){
+  NumericMatrix L = chol_result["L"]; 
+  int n = L.nrow(); 
+  if(b.size() != n) stop("Vector b dimension does not match matrix. "); 
   
+  // ----- Forward Substitution -----
+  NumericVector y(n); 
+  for(int i = 0; i < n; i++){
+    double sum = 0.0; 
+    for(int k = 0; k < i; k++){
+      sum += L(i, k)*y[k]; 
+    }
+    y[i] = (b[i] - sum)/L(i, i); 
+  }
+  
+  // ----- Backward Substitution -----
+  NumericVector x(n);
+  for(int i = n - 1; i >= 0; i--){
+    double sum = 0.0; 
+    for(int k = i + 1; k < n; k++){
+      sum += L(k, i)*x[k]; 
+    }
+    x[i] = (y[i]-sum)/L(i, i); 
+  }
+  
+  return x; 
+}
+
+/**
+ * Calculate Inverse Matrix using Cholesky Decomposition
+ */
+// [[Rcpp::export]]
+NumericMatrix CholInvert(List chol_result){
+  NumericMatrix L = chol_result["L"]; 
+  int n = L.nrow(); 
+  NumericMatrix IA(n, n); 
+  
+  for(int j = 0; j < n; j++){
+    NumericVector b(n); 
+    b[j] = 1.0; 
+    
+    NumericVector y(n);
+    for(int i = 0; i < n; i++){
+      double sum = 0.0; 
+      for(int k = 0; k < i; k++){
+        sum += L(i, k)*y[k]; 
+      }
+      y[i] = (b[i] - sum) / L(i, i); 
+    }
+    
+    for(int i = n - 1; i >= 0; i--){
+      double sum = 0.0; 
+      for(int k = i + 1; k < n; k++){
+        sum += L(k, i) * IA(k, j); 
+      }
+      IA(i, j) = (y[i] - sum) / L(i, i); 
+    }
+  }
+  
+  return IA; 
+}
+
+/**
+ * Calculate Determinant using Cholesky Decomposition
+ * * det(A) = (prod(diag(L)))^2
+ * * @param chol_result List containing "L". 
+ * @return Determinant of A.
+ */
+//[[Rcpp::export]]
+double CholDeterminant(List chol_result){
+  NumericMatrix L = chol_result["L"]; 
+  int n = L.nrow(); 
+  
+  double det_L = 1.0; 
+  for(int i = 0; i < n; i++){
+    det_L *= L(i, i); 
+  }
+  
+  // det(A)=det(L*L^T)=det(L)*det(L^T)=det(L)^2
+  return det_L*det_L; 
+}
